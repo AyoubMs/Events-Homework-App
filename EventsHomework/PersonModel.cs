564dde800a3324be722fa2fb0971c3b94d729268
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:48:53 +0200

    add app

---
+++ EventsHomework/PersonModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventsHomework
+{
+    public class PersonModel
+    {
+        public event EventHandler<string> FullFriends;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int MaxFriends { get; set; }
+        private List<PersonModel> Friends = new List<PersonModel>();
+
+        public PersonModel(string firstName, string lastName, int maxFriends = 0)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MaxFriends = maxFriends;
+        }
+
+        public string AddFriend(PersonModel friend)
+        {
+            string output = "";
+
+            if (Friends.Count < MaxFriends)
+            {
+                Friends.Add(friend);
+                output = $"{friend.FirstName} {friend.LastName} was added to the friends list.";
+                if (Friends.Count == MaxFriends)
+                {
+                    FullFriends?.Invoke(this, $"{FirstName} doesn't need more friends.");
+                }
+            }
+            else
+            {
+                output = $"no more friends.";
+            }
+            return output;
+        }
+    }
+}
---
+++ EventsHomework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel("Ayoub", "Chahir", 3);
+
+            person.FullFriends += Person_FullFriends;
+
+            person.AddFriend(new PersonModel("Salma", "Mitwalli")).PrintToConsole();
+            person.AddFriend(new PersonModel("Salim", "Habadoki")).PrintToConsole();
+            person.AddFriend(new PersonModel("Salim", "Habadok")).PrintToConsole();
+
+            Console.ReadLine();
+        }
+
+        private static void Person_FullFriends(object sender, string e)
+        {
+            Console.WriteLine();
+            Console.WriteLine(e);
+            Console.WriteLine();
+        }
+    }
+}
